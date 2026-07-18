0a9ffa9fa8d731a3f8924c27d677cf3b7de66f84
Chris <ogakris@gmail.com>
2026-07-18 22:08:13 +0100

    sample

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -20,12 +20,31 @@ while (running)
     switch (choice)
     {
         case "1":
+    string prompt = promptGenerator.GetRandomPrompt();
+    Console.WriteLine(prompt);
+
+    Console.Write("> ");
+    string response = Console.ReadLine();
+
+    Entry entry = new Entry();
+    entry._date = DateTime.Now.ToShortDateString();
+    entry._promptText = prompt;
+    entry._entryText = response;
+
+    journal.AddEntry(entry);
             break;
         case "2":
+            journal.DisplayAll();
             break;
         case "3":
+    Console.Write("Enter filename: ");
+    string loadFile = Console.ReadLine();
+    journal.LoadFromFile(loadFile);
             break;
         case "4":
+    Console.Write("Enter filename: ");
+    string saveFile = Console.ReadLine();
+    journal.SaveToFile(saveFile);
             break;
         case "5":
             running = false;
