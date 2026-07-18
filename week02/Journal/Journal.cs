3430ca95fa800c12fa066a85c019691bea7ac598
Chris <ogakris@gmail.com>
2026-07-18 22:20:11 +0100

    sample 4

--- week02/Journal/Journal.cs
+++ week02/Journal/Journal.cs
@@ -13,12 +13,37 @@ public class Journal
             entry.Display();
         }
     }
-    public void SaveToFile(string filename)
+        public void SaveToFile(string filename)
     {
-        
+        using (StreamWriter outputFile = new StreamWriter(filename))
+        {
+            foreach (Entry entry in _entries)
+            {
+                outputFile.WriteLine($"{entry._date}|{entry._promptText}|{entry._entryText}");
+            }
+        }
+
+        Console.WriteLine("Journal saved successfully.");
     }
+
     public void LoadFromFile(string filename)
     {
-        
+        _entries.Clear();
+
+        string[] lines = File.ReadAllLines(filename);
+
+        foreach (string line in lines)
+        {
+            string[] parts = line.Split("|");
+
+            Entry entry = new Entry();
+            entry._date = parts[0];
+            entry._promptText = parts[1];
+            entry._entryText = parts[2];
+
+            _entries.Add(entry);
+        }
+
+        Console.WriteLine("Journal loaded successfully.");
     }
 }
