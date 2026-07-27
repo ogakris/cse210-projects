d35863013b10c522f4db5019ad21d9d80e4ae6bf
Chris <ogakris@gmail.com>
2026-07-27 03:57:28 +0100

    4eg
    Refactored Video class to remove duplicate constructor and methods, and updated member variable names for consistency.

--- week04/OnlineOrdering/Video.cs
+++ week04/OnlineOrdering/Video.cs
@@ -1,46 +1,48 @@
 using System;
 using System.Collections.Generic;
 
-namespace YouTubeVideos
+public class Video
 {
-    public class Video
+    private string _title;
+    private string _author;
+    private int _lengthInSeconds;
+    private List<Comment> _comments;
+
+    public Video(string title, string author, int lengthInSeconds)
     {
-        private string _title;
-        private string _author;
-        private int _lengthSeconds;
-        private List<Comment> _comments;
+        _title = title;
+        _author = author;
+        _lengthInSeconds = lengthInSeconds;
+        _comments = new List<Comment>();
+    }
 
-        public Video(string title, string author, int lengthSeconds)
-        {
-            _title = title;
-            _author = author;
-            _lengthSeconds = lengthSeconds;
-            _comments = new List<Comment>();
-        }
+    public void AddComment(Comment comment)
+    {
+        _comments.Add(comment);
+    }
 
-        public void AddComment(Comment comment)
-        {
-            _comments.Add(comment);
-        }
+    public int GetCommentCount()
+    {
+        return _comments.Count;
+    }
 
-        public int GetCommentCount()
-        {
-            return _comments.Count;
-        }
+    public string GetTitle()
+    {
+        return _title;
+    }
 
-        public void DisplayVideoInfo()
-        {
-            Console.WriteLine($"Title: {_title}");
-            Console.WriteLine($"Author: {_author}");
-            Console.WriteLine($"Length: {_lengthSeconds} seconds");
-            Console.WriteLine($"Number of Comments: {GetCommentCount()}");
-            Console.WriteLine("Comments:");
+    public string GetAuthor()
+    {
+        return _author;
+    }
 
-            foreach (Comment comment in _comments)
-            {
-                Console.WriteLine($"  - {comment.GetName()}: \"{comment.GetText()}\"");
-            }
-            Console.WriteLine(new string('-', 50));
-        }
+    public int GetLengthInSeconds()
+    {
+        return _lengthInSeconds;
+    }
+
+    public List<Comment> GetComments()
+    {
+        return _comments;
     }
 }
