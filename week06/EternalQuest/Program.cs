dcdb8f5caf2f68ff9045b1766d54678fa11d8ec1
Chris <ogakris@gmail.com>
2026-08-08 05:20:36 +0100

    Implement goal management in EternalQuest
    Added functionality to manage and display goals in the EternalQuest project.

--- week06/EternalQuest/Program.cs
+++ week06/EternalQuest/Program.cs
@@ -1,9 +1,45 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the EternalQuest Project.");
+        List<Goal> goals = new List<Goal>();
+
+        goals.Add(new SimpleGoal(
+            "Run a marathon",
+            "Complete a marathon",
+            1000));
+
+        goals.Add(new EternalGoal(
+            "Read scriptures",
+            "Read scriptures every day",
+            100));
+
+        goals.Add(new ChecklistGoal(
+            "Attend the temple",
+            "Attend the temple 10 times",
+            50,
+            10,
+            500));
+
+        foreach (Goal goal in goals)
+        {
+            Console.WriteLine(goal.GetDetailsString());
+        }
+
+        Console.WriteLine();
+
+        int points = goals[0].RecordEvent();
+
+        Console.WriteLine($"You earned {points} points!");
+
+        Console.WriteLine();
+
+        foreach (Goal goal in goals)
+        {
+            Console.WriteLine(goal.GetDetailsString());
+        }
     }
 }
