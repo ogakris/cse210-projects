11b1be1ef12c6e05bd8f7fffb0b1f91f6aca15ab
Chris <ogakris@gmail.com>
2026-08-08 05:19:19 +0100

    Add EternalGoal class for goal management

---
+++ week06/EternalQuest/EternalGoal.cs
@@ -0,0 +1,22 @@
+public class EternalGoal : Goal
+{
+    public EternalGoal(string name, string description, int points)
+        : base(name, description, points)
+    {
+    }
+
+    public override int RecordEvent()
+    {
+        return GetPoints();
+    }
+
+    public override string GetDetailsString()
+    {
+        return $"[ ] {GetName()} - {GetDescription()}";
+    }
+
+    public override bool IsComplete()
+    {
+        return false;
+    }
+}
