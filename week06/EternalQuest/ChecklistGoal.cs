527e06d2213b5330f4c70a29d552d46056dbd925
Chris <ogakris@gmail.com>
2026-08-11 16:01:16 +0100

    4x

--- week06/EternalQuest/ChecklistGoal.cs
+++ week06/EternalQuest/ChecklistGoal.cs
@@ -17,35 +17,55 @@ public class ChecklistGoal : Goal
         _bonus = bonus;
     }
 
-    public override int RecordEvent()
+    public ChecklistGoal(
+        string name,
+        string description,
+        int points,
+        int target,
+        int bonus,
+        int amountCompleted)
+        : base(name, description, points)
     {
-        if (_amountCompleted < _target)
-        {
-            _amountCompleted++;
-
-            int earnedPoints = GetPoints();
-
-            if (_amountCompleted == _target)
-            {
-                earnedPoints += _bonus;
-            }
-
-            return earnedPoints;
-        }
-
-        return 0;
-    }
-
-    public override string GetDetailsString()
-    {
-        string checkbox = IsComplete() ? "[X]" : "[ ]";
-
-        return $"{checkbox} {GetName()} - {GetDescription()} " +
-               $"Completed {_amountCompleted}/{_target}";
+        _target = target;
+        _bonus = bonus;
+        _amountCompleted = amountCompleted;
     }
 
     public override bool IsComplete()
     {
         return _amountCompleted >= _target;
+    }
+
+    public override int RecordEvent()
+    {
+        // Don't allow progress beyond the target.
+        if (IsComplete())
+        {
+            return 0;
+        }
+
+        _amountCompleted++;
+
+        int earnedPoints = GetPoints();
+
+        // Give the bonus when the goal is completed.
+        if (_amountCompleted == _target)
+        {
+            earnedPoints += _bonus;
+        }
+
+        return earnedPoints;
+    }
+
+    public override string GetDetailsString()
+    {
+        return $"{GetStatus()} {GetName()} ({GetDescription()}) " +
+               $"-- Completed {_amountCompleted}/{_target} times";
+    }
+
+    public override string GetSaveString()
+    {
+        return $"Checklist|{GetName()}|{GetDescription()}|{GetPoints()}|" +
+               $"{_target}|{_bonus}|{_amountCompleted}";
     }
 }
