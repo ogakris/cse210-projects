2640d793d0bbd28055ffb84338d08049cd7f3bc0
Chris <ogakris@gmail.com>
2026-08-11 13:59:11 +0100

    4z

---
+++ week07/ExerciseTracking/Running.cs
@@ -0,0 +1,32 @@
+using System;
+
+public class Running : Activity
+{
+    private double _distance;
+
+    public Running(DateTime date, int length, double distance)
+        : base(date, length)
+    {
+        _distance = distance;
+    }
+
+    public override double GetDistance()
+    {
+        return _distance;
+    }
+
+    public override double GetSpeed()
+    {
+        return (GetDistance() / GetLength()) * 60;
+    }
+
+    public override double GetPace()
+    {
+        return GetLength() / GetDistance();
+    }
+
+    protected override string GetActivityName()
+    {
+        return "Running";
+    }
+}
