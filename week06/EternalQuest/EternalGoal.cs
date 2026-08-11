8e7bbdc82b34f6267c7cebfa17dc186d9bfe4a52
Chris <ogakris@gmail.com>
2026-08-11 16:02:19 +0100

    5x

--- week06/EternalQuest/EternalGoal.cs
+++ week06/EternalQuest/EternalGoal.cs
@@ -1,22 +1,43 @@
 public class EternalGoal : Goal
 {
+    private int _timesCompleted;
+
     public EternalGoal(string name, string description, int points)
         : base(name, description, points)
     {
+        _timesCompleted = 0;
+    }
+
+    public EternalGoal(
+        string name,
+        string description,
+        int points,
+        int timesCompleted)
+        : base(name, description, points)
+    {
+        _timesCompleted = timesCompleted;
+    }
+
+    public override bool IsComplete()
+    {
+        // Eternal goals are never complete.
+        return false;
     }
 
     public override int RecordEvent()
     {
+        _timesCompleted++;
         return GetPoints();
     }
 
     public override string GetDetailsString()
     {
-        return $"[ ] {GetName()} - {GetDescription()}";
+        return $"{GetStatus()} {GetName()} ({GetDescription()}) " +
+               $"-- Completed {_timesCompleted} times";
     }
 
-    public override bool IsComplete()
+    public override string GetSaveString()
     {
-        return false;
+        return $"Eternal|{GetName()}|{GetDescription()}|{GetPoints()}|{_timesCompleted}";
     }
 }
