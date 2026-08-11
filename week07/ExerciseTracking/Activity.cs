f905a4fc8a523ee20fa831e0a30743a4b9c8f689
Chris <ogakris@gmail.com>
2026-08-11 14:01:13 +0100

    5z

---
+++ week07/ExerciseTracking/Activity.cs
@@ -0,0 +1,40 @@
+using System;
+
+public abstract class Activity
+{
+    private DateTime _date;
+    private int _length;
+
+    public Activity(DateTime date, int length)
+    {
+        _date = date;
+        _length = length;
+    }
+
+    public DateTime GetDate()
+    {
+        return _date;
+    }
+
+    public int GetLength()
+    {
+        return _length;
+    }
+
+    // These methods are declared but not implemented.
+    public abstract double GetDistance();
+
+    public abstract double GetSpeed();
+
+    public abstract double GetPace();
+
+    public virtual string GetSummary()
+    {
+        return $"{_date:dd MMM yyyy} {GetActivityName()} ({_length} min) - " +
+               $"Distance: {GetDistance():0.0} miles, " +
+               $"Speed: {GetSpeed():0.0} mph, " +
+               $"Pace: {GetPace():0.0} min per mile";
+    }
+
+    protected abstract string GetActivityName();
+}
