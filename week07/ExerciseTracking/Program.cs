3a5e5589bfee343d305194a8d0b0d991368620ee
Chris <ogakris@gmail.com>
2026-08-11 13:55:14 +0100

    1z

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -1,9 +1,37 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        List<Activity> activities = new List<Activity>();
+
+        Running running = new Running(
+            new DateTime(2026, 11, 8),
+            30,
+            3.0
+        );
+
+        Cycling cycling = new Cycling(
+            new DateTime(2026, 11, 8),
+            30,
+            12.0
+        );
+
+        Swimming swimming = new Swimming(
+            new DateTime(2026, 11, 8),
+            30,
+            40
+        );
+
+        activities.Add(running);
+        activities.Add(cycling);
+        activities.Add(swimming);
+
+        foreach (Activity activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
